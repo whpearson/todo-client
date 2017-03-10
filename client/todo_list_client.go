0906a1ef65459e06c9c24026b5a0182921810405
Will Pearson <will.pearson@digital.cabinet-office.gov.uk>
2017-03-10 20:02:20 +0000

    Update the client reference to be correct

--- client/todo_list_client.go
+++ client/todo_list_client.go
@@ -9,7 +9,7 @@ import (
 
 	strfmt "github.com/go-openapi/strfmt"
 
-	"github.com/go-swagger/go-swagger/examples/todo-list/client/todos"
+	"github.com/whpearson/todo-client/client/todos"
 )
 
 // Default todo list HTTP client.
